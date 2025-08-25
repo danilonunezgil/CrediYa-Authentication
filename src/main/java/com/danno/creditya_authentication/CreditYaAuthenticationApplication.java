1fb7f12d5fe04874c21525296e2c5671c96d8949
Danilo Núñez Gil <danilo.gil98@gmail.com>
2025-08-25 09:34:26 -0500

    🛠️Maint(Base Project): Initial Structure for microservice

---
+++ src/main/java/com/danno/creditya_authentication/CreditYaAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.danno.creditya_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditYaAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditYaAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danno/creditya_authentication/CreditYaAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danno.creditya_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditYaAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
